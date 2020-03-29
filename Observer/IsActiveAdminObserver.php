e0bb6ca06afe9849aefc63f475cb9246a652553d
mongkok <domake.io@gmail.com>
2020-03-29 17:29:45 +0700

    Added and improved observers to check payment method availability

--- Observer/IsActiveAdminObserver.php
+++ Observer/IsActiveAdminObserver.php
@@ -7,24 +7,13 @@ namespace Postpay\Payment\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Postpay\Payment\Gateway\Config\Config;
 use Postpay\Payment\Model\Postpay;
 
 /**
- * Class IsActiveObserver
+ * Class IsActiveAdminObserver
  */
-class IsActiveObserver implements ObserverInterface
+class IsActiveAdminObserver implements ObserverInterface
 {
-    /**
-     * Constructor.
-     *
-     * @param Config $config
-     */
-    public function __construct(Config $config)
-    {
-        $this->config = $config;
-    }
-
     /**
      * @inheritdoc
      */
@@ -32,8 +21,8 @@ class IsActiveObserver implements ObserverInterface
     {
         $event = $observer->getEvent();
         $methodInstance = $event->getMethodInstance();
-
-        if ($methodInstance instanceof Postpay && !$this->config->isAvailable()) {
+        
+        if ($methodInstance instanceof Postpay) {
             /** @var \Magento\Framework\DataObject $result */
             $result = $observer->getEvent()->getResult();
             $result->setData('is_available', false);
